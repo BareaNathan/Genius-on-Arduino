553b563faff75cc9d431b031c12ad39ca6831bae
Nathan <nathanszekutb@hotmail.com>
2022-09-14 21:35:27 -0300

    Cleaning the code, ending the first step of the game, with a simple blink led at the digital port 2

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -1,20 +1,18 @@
-//Porta 2 não deve ser variável, correção para definir valor fixo de redLed
-//int redLed = 2;
+
 #define REDLED 2
 #define SEC_1 1000
 #define SEC_05 500
 
 void setup() {
-  // put your setup code here, to run once:
+  
   Serial.begin(9600);
   pinMode(REDLED, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  
   Serial.println(REDLED);
   blink(REDLED);
-  //redLed++;
 }
 
 void blink(int ledPort){
