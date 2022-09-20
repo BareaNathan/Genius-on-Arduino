107106a4487950b7ff874a7c44542ef17032fad7
Nathan <nathanszekutb@hotmail.com>
2022-09-19 21:07:46 -0300

    Reorganizing the Setup and Loop, adding 3 new led collors, configure test sequence and starting the game initializer

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -1,18 +1,49 @@
 
-#define REDLED 2
+#define GREENLED 2
+#define YELLOWLED 3
+#define REDLED 4
+#define BLUELED 5
+
 #define SEC_1 1000
 #define SEC_05 500
+
+#define SEQUENCE_SIZE 4
+
+int lightSequence[SEQUENCE_SIZE];
 
 void setup() {
   
   Serial.begin(9600);
+  portConfig();
+  gameStart();
+}
+
+void gameStart(){
+  lightSequence[0] = BLUELED;
+  lightSequence[1] = GREENLED;
+  lightSequence[2] = REDLED;
+  lightSequence[3] = YELLOWLED;
+}
+
+void portConfig(){
+  pinMode(GREENLED, OUTPUT);
+  pinMode(YELLOWLED, OUTPUT);
   pinMode(REDLED, OUTPUT);
+  pinMode(BLUELED, OUTPUT);
 }
 
 void loop() {
-  
-  Serial.println(REDLED);
+  for(int index = 0; index < SEQUENCE_SIZE; index++){
+    blink(lightSequence[index]);
+  }
+}
+
+void testSequence1(){
+  blink(GREENLED);
+  blink(YELLOWLED);
   blink(REDLED);
+  blink(BLUELED);
+  delay(SEC_05);
 }
 
 void blink(int ledPort){
